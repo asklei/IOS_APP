387b5d1c3154c9bd8e870c229fd5768d5a63b293
Lei Xu <leix@uber.com>
2016-07-20 11:53:43 -0700

    Add Notes project

---
+++ Notes/Notes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by Lei Xu on 7/20/16.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notes/Notes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notes
+//
+//  Created by Lei Xu on 7/20/16.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
